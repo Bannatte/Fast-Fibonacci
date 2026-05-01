7c6a7dceb411039e56420653b973f172d517228e
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-05-01 13:18:18 -0700

    Refactor div_sqrt_five to use vectors and long long

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
 int div_sqrt_five (int num)
 {
   int low = num / 3;
   int upp = num / 2;
 
-  int target = num * num;
+  long long target = num * num;
   while (low <= upp) 
   {
     int mid = (low + upp) / 2;
-    int value = 5 * mid * mid;
+    long long value = 5 * mid * mid;
 
     if (value == target) {
       return mid;
@@ -27,27 +31,25 @@ int fast_fib (int num) {
     highest_bit--;
   }
 
-  int p_array[highest_bit];
-  p_array[0] = 1;
-  int q_array[highest_bit];
-  q_array[0] = 1;
+  vector<int> p = {1};
+  vector<int> q = {1};
 
   for (int i = 0; i <= highest_bit; i++) {
-    p_array[i+1] = p_array[i] * p_array[i] + 5 * q_array[i] * q_array[i];
-    q_array[i+1] = 2 * p_array[i] * q_array[i];
+    p.push_back(p[i] * p[i] + 5 * q[i] * q[i]);
+    q.push_back(2 * p[i] * q[i]);
   }
   
   int p_tot = 1;
   int q_tot = 0;
   for (int j = highest_bit; 0 <= j; j--) {
-    int b = (num >> i) & 1;
+    int b = (num >> j) & 1;
 
-    if (b == 0) {
+    if (b != 1) {
       continue;
     }
 
-    int p_tot_new = p_array[j] * p_tot + q_array[j] * q_tot * 5;
-    q_tot = q_array[j] * p_tot + p_array[j] * q_tot;
+    int p_tot_new = p[j] * p_tot + q[j] * q_tot * 5;
+    q_tot = q[j] * p_tot + p[j] * q_tot;
     p_tot = p_tot_new;
   }
 
